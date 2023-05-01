bce5030836f868db54dbb62ff797119c417d4dd0
ErzaBlakajj <eb57603@ubt-uni.net>
2023-05-01 22:06:24 +0200

    commit deleteInfromacionet

--- lab1-project/Services/InfromacionetService.cs
+++ lab1-project/Services/InfromacionetService.cs
@@ -55,6 +55,61 @@ namespace lab1_project.Services
                 return result.ToList();
             }
         }
+        public void DeleteInfromacionet(int id)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        var parameters = new DynamicParameters();
+                        parameters.Add("@Id", id);
+
+                        connection.Execute("DeleteInfromacionet", parameters, commandType: CommandType.StoredProcedure, transaction: transaction);
+
+                        transaction.Commit();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+        public void UpdateInfromacionet(int? id, string title, string description, string username ,int? lineId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        var parameters = new Dapper.DynamicParameters();
+                        parameters.Add(@"Emri_Perdoruesit", username);
+                        parameters.Add("@Id", id);
+                        parameters.Add("@Titulli", title);
+                        parameters.Add("@Pershkrimi", description);
+                        parameters.Add("@Id_Linjat", lineId);
+
+
+
+                        connection.Execute("UpdateInfromacionet", parameters, commandType: CommandType.StoredProcedure, transaction: transaction);
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
 
 
     }
