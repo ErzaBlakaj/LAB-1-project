9181b7b51e3306e0d220bd47d2e5eb85d59d1945
ErzaBlakajj <eb57603@ubt-uni.net>
2023-05-01 20:33:31 +0200

    commit

---
+++ lab1-project/Services/InfromacionetService.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using lab1_project.Models;
+
+namespace lab1_project.Services
+{
+    public class InfromacionetService
+    {
+        private string _connectionString { get; set; }
+
+        public InfromacionetService(string connectionString)
+        {
+            _connectionString = connectionString;
+
+        }
+        public void InsertInfromacionet(string title, string description,string username, int? lineId)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                using (var transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        var parameters = new Dapper.DynamicParameters();
+                        parameters.Add("@Emri_Perdoruesit", username);
+                        parameters.Add("@Titulli", title);
+                        parameters.Add("@Pershkrimi", description);
+                        parameters.Add("@Id_Linjat", lineId);
+
+                        connection.Execute("InsertInfromacionet", parameters, commandType: CommandType.StoredProcedure, transaction: transaction);
+
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
