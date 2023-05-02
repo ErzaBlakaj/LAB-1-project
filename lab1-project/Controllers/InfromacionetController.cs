82b5668935a514e5adb9a76b2fd4ddbe7d422e4e
ErzaBlakajj <eb57603@ubt-uni.net>
2023-05-02 21:56:51 +0200

    InformacionetController

--- lab1-project/Controllers/InfromacionetController.cs
+++ lab1-project/Controllers/InfromacionetController.cs
@@ -27,7 +27,48 @@ namespace lab1_project.Controllers
             {
                 _InfromacionetService.InsertInfromacionet(infromacionet.Titulli, infromacionet.Pershkrimi);
 
-                return "Njoftimi u  krijua me sukses!";
+                return "Informacioni u  krijua me sukses!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [HttpDelete("InfromacionetById")]
+        public string DeleteNjoftimById([FromQuery] int infromacionetId)
+        {
+            try
+            {
+                _infromacionetService.DeleteInfromacionet(infromacionetId);
+
+                return "Informacioni u fshi  me sukses!";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        [HttpGet("getAllInfromacionet")]
+        public List<GetNjoftimet> GetAllInfromacionet()
+        {
+            try
+            {
+
+                return _infromacionetService.GetInfromacionet();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpPut("updateInfromacionetById")]
+        public string UpdateInfromacionetById([FromBody] Infromacionet infromacionet)
+        {
+            try
+            {
+                _InfromacionetService.InsertInfromacionet(infromacionet.Titulli, infromacionet.Pershkrimi);
+
+                return "Infromacioni u perditesua me sukses!";
             }
             catch (Exception ex)
             {
